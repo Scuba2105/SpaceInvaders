5334a4045899f7822f5e79c15b64c6be0ae52b74
Steven Bradbury <scuba2150@gmail.com>
2024-05-25 22:58:53 +1000

    refactored code

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,17 +15,48 @@ class MainGame():
     def run_game(self):
         # Start the main loop for the game
         while True:
-            # Watch for keyboard and mouse events as user input
+            self._check_events()
+            self._update_screen()
+            self.ship.update()
+            
+    def _check_events(self):
+        # Watch for keyboard and mouse events as user input
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit 
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        # Move the ship to the right
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        # Move the ship to the left
+                        self.ship.moving_left = True
+                    elif event.key == pygame.K_UP:
+                        self.ship.moving_up = True
+                    elif event.key == pygame.K_DOWN:
+                        self.ship.moving_down = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        # Move the ship to the right
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        # Move the ship to the left
+                        self.ship.moving_left = False
+                    elif event.key == pygame.K_UP:
+                        # Move the ship up (Forward)
+                        self.ship.moving_up = False
+                    elif event.key == pygame.K_DOWN:
+                        # Move the ship down (Reverse)
+                        self.ship.moving_down = False
 
-            # Redraw the screen during each pass through the loop to maintain bg color
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+    
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop to maintain bg color
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()            
+        
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == "__main__":
     # Make a game instance and run the game
--- ship.py
+++ ship.py
@@ -1,20 +1,39 @@
 import pygame
+from settings import Settings 
 
 class Ship:
     # A class to manage the players ship
     def __init__(self, ai_game):
-        print(ai_game)
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
+        # Ship settings
+        self.settings = ai_game.settings
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
         # Load the ship and get its rect
         self.image = pygame.image.load("images/ship.bmp")
-        self.image = pygame.transform.scale(self.image, (120, 160))
+        self.image = pygame.transform.scale(self.image, (90, 130))
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
-        
+    
+    def update(self):
+        if self.moving_right:
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left:
+            self.rect.x -= self.settings.ship_speed 
+        if self.moving_up:
+            self.rect.y -= self.settings.ship_speed
+        if self.moving_down:
+            self.rect.y += self.settings.ship_speed
+
     def blitme(self):
         # Draw the ship to the main screen (self.screen) at its current location
         self.screen.blit(self.image, (self.rect.x, self.rect.y))
